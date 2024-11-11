7fb500d3b06c69ddf07eda5d3c28c4a4d43982b4
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-11 13:09:55 +0530

    Created readFile method

--- Node.js/fsModule/index.js
+++ Node.js/fsModule/index.js
@@ -3,4 +3,9 @@ const fs  = require('fs');
 fs.writeFile("abcd.txt","Hello world", function(err){
   if(err)  console.log(err);
   console.log("The file was saved!");
-});
+}); 
+
+fs.readFile("abcd.txt",'utf-8',function(err,data){
+  if(err) console.log(err);
+  console.log(data);
+})
