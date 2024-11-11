e87d27c351ba9acb5591487c232fee2b030463bb
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-11 13:19:39 +0530

    Created method to delete file

--- Node.js/fsModule/index.js
+++ Node.js/fsModule/index.js
@@ -1,21 +1,31 @@
 const fs  = require('fs');
 
+// to create a new file
 fs.writeFile("abcd.txt","Hello world", function(err){
   if(err)  console.log(err);
   console.log("The file was saved!");
 }); 
 
+// to read file content
 fs.readFile("abcd.txt",'utf-8',function(err,data){
   if(err) console.log(err);
   console.log(data);
 })
 
+// to append data to the file
 fs.appendFile("abcd.txt","appended data",function(err){
   if(err) console.log(err);
   console.log(`data appended`)
 })
 
+// to rename file
 fs.rename("abcd.txt","newFile.txt",function(err){
   if(err) console.log(err);
   console.log(`File renamed`);
+})
+
+// To delete file
+fs.unlink("newFile.txt",function(err){
+  if(err) console.log(err);
+  console.log(`File deleted`);
 })
