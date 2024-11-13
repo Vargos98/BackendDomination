7cf307ff5e132f45b2960a40fe6b70480887c0ba
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-13 13:36:34 +0530

    Update index.js
    Created routings in the server for different routes

--- Node.js/HttpModule/index.js
+++ Node.js/HttpModule/index.js
@@ -1,7 +1,21 @@
 const http = require('http');
 port  = 3000;
+// var server = http.createServer((req,res)=>{
+//    res.end("Are you serving this")
+// })
+
+
 var server = http.createServer((req,res)=>{
-   res.end("Are you serving this")
+    if(req.url === '/'){
+        res.end("Welcome to the homepage!");
+    }
+    else if(req.url === '/profile'){
+        res.end("Welcome to profile!");    
+    }
+    else if(req.url=== '/user'){
+        res.end("Welcome to user!");
+    }
 })
+
 
 server.listen(port);
