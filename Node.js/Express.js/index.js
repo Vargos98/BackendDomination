4029eda2793438df6edc67b2d310bb1db851fdff
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-14 11:33:15 +0530

    Update index.js
    Created sessions

--- Node.js/Express.js/index.js
+++ Node.js/Express.js/index.js
@@ -2,20 +2,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware definations 
+const expressSession = require('express-session');
 
-app.use((req,res,next)=>{
-    console.log("Hey im the middleware!");
-    next();
-})
+app.use(expressSession({
+    secret:"Random texts", // the secret should be unique and because of this only the whole code in encrypted.
+    resave: false,
+    saveUninitialized: false,
+}))
 
 app.get('/', (req,res)=>{
     res.send("Hello, world!");
 })
 
-app.get('/about', (req,res)=>{
-    res.send("About page");
+app.get('/create', (req,res, next)=>{
+    req.session.polo = true;
+    res.send("done");
+    next();
 })
+app.get('/check', (req,res, next)=>{
+    console.log(req.session.polo);
+    
+})
+
+
+// Middleware definations 
+
+// app.use((req,res,next)=>{
+//     console.log("Hey im the middleware!");
+//     next();
+// })
+
+
+
+// app.get('/about', (req,res)=>{
+//     res.send("About page");
+// })
 
 
 app.listen(port)
