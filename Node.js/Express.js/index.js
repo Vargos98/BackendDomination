6854aa205912c51b6cced503c4d0fcace0536ea6
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-14 11:11:42 +0530

    Update index.js
    Created  a middleware which can be used for security, data management/organization, for JWT auth or google auth. etc etc

--- Node.js/Express.js/index.js
+++ Node.js/Express.js/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Middleware definations 
+
+app.use((req,res,next)=>{
+    console.log("Hey im the middleware!");
+    next();
+})
+
 app.get('/', (req,res)=>{
     res.send("Hello, world!");
 })
