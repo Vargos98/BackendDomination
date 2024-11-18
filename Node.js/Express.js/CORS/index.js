6303078d427b0d5eb83d51a76de886fde1d1bc63
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-18 10:12:15 +0530

    Update index.js
    implemented single shareable route

--- Node.js/Express.js/CORS/index.js
+++ Node.js/Express.js/CORS/index.js
@@ -7,6 +7,17 @@
  app.use(cors()); // enable CORS middleware
 
 // Now we can define as many routes as we want and CORS will allow access to all of them to another domain.
+
+
+
+//  Also if we want to share a data from a specific route only then we can use this 
+
+app.get('/shareable',cors(),(req,res)=>{
+  res.send("shareable data");
+})
+
+
+
  app.get('/', (req,res)=>{
    res.send('Hello from Express.js');
  })
