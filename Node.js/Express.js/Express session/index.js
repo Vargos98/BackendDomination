832c774cf30c45cef56e697190024e593ecd6ba0
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-18 09:44:47 +0530

    Create index.js
    implemented express-session

---
+++ Node.js/Express.js/Express session/index.js
@@ -0,0 +1,37 @@
+const expressSession = require('express-session');
+const express = require('express');
+const app = express();
+
+const port = 3000; // set the port
+
+// Session middleware
+app.use(expressSession({
+  secret: "Random texts", // the secret should be unique and secure
+  resave: false,
+  saveUninitialized: false,  // If a session does not exist, it will not be saved to the store.
+}));
+
+// Root route
+app.get('/', (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Create session variable
+app.get('/create', (req, res) => {
+  req.session.polo = true; // Set a session variable
+  res.send("Session variable 'polo' set to true!");
+});
+
+// Check session variable
+app.get('/check', (req, res) => {
+  const poloValue = req.session.polo;
+  console.log("Session polo:", poloValue);
+  if (poloValue) {
+    res.send(`Session variable 'polo' exists and is ${poloValue}`);
+  } else {
+    res.send("Session variable 'polo' is undefined.");
+  }
+});
+
+// Start the server
+app.listen(port);
