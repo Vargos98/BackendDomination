d74fc41cc8b17965939625746b634e5a17c53bd3
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-24 11:48:05 +0530

    Update index.js
    Created express-session in the server to store user data and maintain the user details and see if the user is being stored or not

--- Express.js/Express session/index.js
+++ Express.js/Express session/index.js
@@ -1,37 +1,63 @@
-const expressSession = require('express-session');
+// const expressSession = require('express-session');
+// const express = require('express');
+// const app = express();
+
+// const port = 3000; // set the port
+
+// // Session middleware
+// app.use(expressSession({
+//   secret: "Random texts", // the secret should be unique and secure
+//   resave: false,
+//   saveUninitialized: false,  // If a session does not exist, it will not be saved to the store.
+// }));
+
+// // Root route
+// app.get('/', (req, res) => {
+//   res.send("Hello, world!");
+// });
+
+// // Create session variable
+// app.get('/create', (req, res) => {
+//   req.session.polo = true; // Set a session variable
+//   res.send("Session variable 'polo' set to true!");
+// });
+
+// // Check session variable
+// app.get('/check', (req, res) => {
+//   const poloValue = req.session.polo;
+//   console.log("Session polo:", poloValue);
+//   if (poloValue) {
+//     res.send(`Session variable 'polo' exists and is ${poloValue}`);
+//   } else {
+//     res.send("Session variable 'polo' is undefined.");
+//   }
+// });
+
+// // Start the server
+// app.listen(port);
+
 const express = require('express');
+const expressSession = require('express-session');
+const PORT = 3000;
 const app = express();
 
-const port = 3000; // set the port
-
-// Session middleware
 app.use(expressSession({
-  secret: "Random texts", // the secret should be unique and secure
+  secret :"random exncryption key",
   resave: false,
-  saveUninitialized: false,  // If a session does not exist, it will not be saved to the store.
+  saveUninitialized : false,
 }));
 
-// Root route
-app.get('/', (req, res) => {
-  res.send("Hello, world!");
-});
 
-// Create session variable
-app.get('/create', (req, res) => {
-  req.session.polo = true; // Set a session variable
-  res.send("Session variable 'polo' set to true!");
-});
+app.get("/create", (req,res,next)=>{
+  req.session.name = true;
+  res.send("done");
+  next();
 
-// Check session variable
-app.get('/check', (req, res) => {
-  const poloValue = req.session.polo;
-  console.log("Session polo:", poloValue);
-  if (poloValue) {
-    res.send(`Session variable 'polo' exists and is ${poloValue}`);
-  } else {
-    res.send("Session variable 'polo' is undefined.");
-  }
-});
+})
 
-// Start the server
-app.listen(port);
+app.get('/check', (req,res,next)=>{
+  console.log(req.session.name);
+  next()
+})
+
+app.listen(PORT);
