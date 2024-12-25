fd527aa040733813d14d4f4f6fc94af1b697bfb7
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-25 12:21:12 +0530

    Update app.js
    Implemented different routes sharing list of users, also list of number and implemented CRUD operations on it

--- Express.js/Postman/app.js
+++ Express.js/Postman/app.js
@@ -2,9 +2,56 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+const users = [
+  {
+    id: 1,
+    name: 'John',
+    age: 25,
+    email: 'john@example.com'
+  },
+  {
+    id: 2,
+    name: 'Alice',
+    age: 30,
+    email: 'alice@example.com'
+  },
+  {
+    id: 3,
+    name: 'Bob',
+    age: 28,
+    email: 'bob@example.com'
+  }
+];
+const data = [1,2,3,4,5,];
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('Hello from Express.js');
+});
+
+app.get('/profile', (req, res) => {
+  // Send the users array as a JSON response
+  res.json({
+    message: 'List of users',
+    users: users
+  });
+});
+
+
+
+app.get('/data',(req,res)=>{
+  res.send(data);
 })
 
-app.listen(PORT);
+app.post('/data/:number',(req,res)=>{
+  data.push(parseInt(req.params.number));
+  res.send(data);
+})
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
