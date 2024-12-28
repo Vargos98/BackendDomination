acb928393c6c9f51a08f573e4614a0cb2a230a1d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-28 11:33:47 +0530

    Updated url

--- FullStack-Project/Frontend/src/App.jsx
+++ FullStack-Project/Frontend/src/App.jsx
@@ -1,20 +1,26 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
 const [jokes, setJokes] = useState([]);
+useEffect(()=>{
+  axios.get('http://localhost:3000/api/jokes')
+ .then(res=> setJokes(res.data))
+ .catch(err=>console.error(err))
+})
 
   return (
     <>
       <h1>Hey There!</h1>
       <p>number of Jokes : {jokes.length}</p>
       {
-        jokes.map((joke,index)=>{
+        jokes.map((joke)=>{
 
-          <div key={joke.id}>
+          <div key={joke.id} >
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
           </div>
