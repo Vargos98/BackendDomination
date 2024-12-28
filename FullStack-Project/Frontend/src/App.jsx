af5e524ea09a1a115e6bcbcf485335fa2e4c2512
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-28 11:38:03 +0530

    Created proxy url in the vite.config file to limit URL length

--- FullStack-Project/Frontend/src/App.jsx
+++ FullStack-Project/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 function App() {
 const [jokes, setJokes] = useState([]);
 useEffect(()=>{
-  axios.get('http://localhost:3000/api/jokes')
+  axios.get('/api/jokes')
  .then(res=> setJokes(res.data))
  .catch(err=>console.error(err))
 })
