77bee0becfdf0f20eb81d6703a274e7386074e98
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-06 14:09:12 +0530

    Completed JOI setup

--- JOI/App.js
+++ JOI/App.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+const {userModel, validateModel }= require('./models/user.model.js');
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/create', async (req, res) => {
+  const { name, username, age, contact, email } = req.body;
+  validateModel({name, username, age, contact,email});
+  res.send("check console");
 });
 
 app.listen(PORT, () => {
--- JOI/models/user.model.js
+++ JOI/models/user.model.js
@@ -1,3 +1,54 @@
 const mongoose = require('mongoose');
-
+const Joi = require('joi');
 mongoose.connect('mongodb+srv://vargos98:9828206Uk@cluster0.z5ygt.mongodb.net/')
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+
+  .catch((err) => {
+    console.error(err)
+  })
+
+
+const userSchema = mongoose.Schema({
+  username : {
+    type:String,
+    required: true,
+    unique: true,
+    minlength: 3,
+
+  },
+  name :{
+    type: String,
+    required: true,
+  },
+  email:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age:{
+    type: Number,
+    required: true,
+    min:18,
+  },
+  contact :{
+    type: String,
+    required: true,
+  },
+});
+
+
+function validateModel(data){
+  let schema = Joi.object({
+    username: Joi.string().min(3).required(),
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    age: Joi.number().min(18).required(),
+    contact: Joi.string().required(),
+  })
+  let result = schema.validate(data);
+  return result;
+}
+let userModel = mongoose.model("User", userSchema);
+module.exports = {validateModel, userModel}
