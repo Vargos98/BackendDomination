b987b0f52ecd5851e329326373509d2b2b6bf91c
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-07 11:15:45 +0530

    Auth boilerplate

---
+++ Auth/App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+
+
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+  res.send(`Welcome`);
+});
+
+app.post('/encrypt', async (req, res) => {
+ let salt =await bcrypt.genSalt(10)
+ res.send(salt);
+});
+
+
+app.listen(()=>{
+  console.log(`Server is running on port ${port}`);
+});
