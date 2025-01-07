ddbfe07a75b16ca24e263ce3f91323981da4161d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-07 11:18:34 +0530

    Update App.js

--- Auth/App.js
+++ Auth/App.js
@@ -2,20 +2,17 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
 
-
 const port = 5000;
-
 
 app.get('/', (req, res) => {
   res.send(`Welcome`);
 });
 
 app.post('/encrypt', async (req, res) => {
- let salt =await bcrypt.genSalt(10)
- res.send(salt);
+  let salt = await bcrypt.genSalt(10);
+  res.send(salt);
 });
 
-
-app.listen(()=>{
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
