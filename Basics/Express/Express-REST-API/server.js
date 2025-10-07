3d8ca41687a23a1f039f0c889a887a9c1cbcf79d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-10-07 12:56:51 +0530

    Add RESTful routes for user operations
    Introduced GET, PUT, and DELETE handlers for '/api/users/:id' to retrieve, update (pending), and delete (pending) users by ID. Improved route organization and clarified comments for user-related endpoints.

--- Basics/Express/Express-REST-API/server.js
+++ Basics/Express/Express-REST-API/server.js
@@ -1,8 +1,8 @@
 // REST API- JSON
 
-// GET/users - list all users
-// GET/users/1 - Get the user with ID 1
-// POST/users - Create a new user
+
+
+
 //PATCH /users/1 - Edit the user with ID 1
 //DELETE/users/1 - Delete the user with ID 1
 
@@ -14,6 +14,19 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 const users = require("./MOCK_DATA.json")
+
+app.route("/api/users/:id").get((req,res)=>{
+  const id = Number(req.params.id);
+  const user = users.find(user=> user.id === id)
+  return res.json(user);
+})
+.put((req,res)=>{
+  return res.json({status:"Pending"});
+})
+.delete((req,res)=>{
+  return res.json({status:"Pending"})
+})
+
 
 // this route will provide an HTML doc response
 app.get("/users", (req, res) => {
@@ -29,10 +42,17 @@ app.get("/users", (req, res) => {
   res.send(html);
 });
 
+
+// GET/users - list all users
 // This route will provide an json response
 app.get("/api/users", (req,res)=>{
   return res.json(users);
 })
+// GET/users/1 - Get the user with ID 1
+
+
+// POST/users - Create a new user
+
 
 
 app.listen(PORT,()=>{
