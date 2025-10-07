9375c934a06bb0ce7b782cf8bd251d4c58391481
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-10-07 12:03:35 +0530

    Add mock user data and update server logic
    Added MOCK_DATA.json containing user data for the Express REST API. Updated server.js to utilize the mock data, enabling API endpoints to serve realistic user information for development and testing.

--- Basics/Express/Express-REST-API/server.js
+++ Basics/Express/Express-REST-API/server.js
@@ -1,9 +1,37 @@
+// REST API- JSON
+
+// GET/users - list all users
+// GET/users/1 - Get the user with ID 1
+// POST/users - Create a new user
+//PATCH /users/1 - Edit the user with ID 1
+//DELETE/users/1 - Delete the user with ID 1
+
+
+
+
+
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const users = require("./MOCK_DATA.json")
 
-app.get("/", (req,res)=>{
-  return res.send("Hello from express server");
+// this route will provide an HTML doc response
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+      ${users.map(user => `
+        <li>
+          ${user.id} - ${user.first_name} ${user.last_name} - ${user.email} - ${user.gender} - ${user.job_title}
+        </li>
+      `).join('')}
+    </ul>
+  `;
+  res.send(html);
+});
+
+// This route will provide an json response
+app.get("/api/users", (req,res)=>{
+  return res.json(users);
 })
 
 
